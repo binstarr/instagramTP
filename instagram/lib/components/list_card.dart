e61f5a0ad5b32673677d62be5f1a4f50183b7b63
1000bang <1000bang@1000bangui-MacBookAir.local>
2022-11-27 23:21:00 +0900

    11/27 dm page updated

--- instagram/lib/components/list_card.dart
+++ instagram/lib/components/list_card.dart
@@ -1,11 +1,8 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instagram/components/circle_Image.dart';
 import 'package:instagram/main.dart';
 import 'package:intl/intl.dart';
-
 
 import '../constrants.dart';
 import '../model/user_info.dart';
@@ -13,7 +10,15 @@ import '../screens/my_profile.dart';
 import 'my_comment.dart';
 
 class ListCard extends StatefulWidget {
-  ListCard({Key? key, required this.profileimg, required this.name, required this.mainImg, required this.likes, required this.content, required this.time}) : super(key: key);
+  ListCard(
+      {Key? key,
+      required this.profileimg,
+      required this.name,
+      required this.mainImg,
+      required this.likes,
+      required this.content,
+      required this.time})
+      : super(key: key);
   String profileimg;
   String name;
   String mainImg;
@@ -29,95 +34,114 @@ class ListCardState extends State<ListCard> {
   var a = true;
   List<MyComment> comments = [];
   TextEditingController _textEditingController = new TextEditingController();
+  int alreadySaved = 0;
 
   @override
   Widget build(BuildContext context) {
-
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 6.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _firstRow(),
-          SizedBox(height: 10,),
-          Center(child: Image.network(widget.mainImg, fit: BoxFit.contain,)),
-          _secondRow(),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+              child: Image.network(
+            widget.mainImg,
+            fit: BoxFit.contain,
+          )),
+          _secondRow(0),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Text("좋아요 ${widget.likes} 개", style: TextStyle(fontWeight: FontWeight.bold), ),
+            child: Text(
+              "좋아요 ${widget.likes} 개",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
           _thirdRow(),
           ...List.generate(comments.length, (index) => comments[index]),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           _fourthRow(),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text("${widget.time} 시간 전", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10, color: Colors.grey), ),),
-          SizedBox(height: 10,),
-
-
-
-
-
+            child: Text(
+              "${widget.time} 시간 전",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 10,
+                  color: Colors.grey),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
         ],
-
       ),
     );
   }
 
-  _firstRow(){
+  _firstRow() {
     return Row(
       children: [
         InkWell(
           onTap: () {
-            if(widget.name == "binstar") {
+            if (widget.name == "binstar") {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (context) =>
-                  MyProfile(user: user[2])));
-            }else  if(widget.name == "jieun") {
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MyProfile(user: user[2])));
+            } else if (widget.name == "jieun") {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (context) =>
-                  MyProfile(user: user[1])));
-            }else if(widget.name == "swlee") {
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MyProfile(user: user[1])));
+            } else if (widget.name == "swlee") {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (context) =>
-                  MyProfile(user: user[3])));
-            }else if(widget.name == "1000bang") {
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MyProfile(user: user[3])));
+            } else if (widget.name == "1000bang") {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (context) =>
-                  MyProfile(user: user[0])));
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MyProfile(user: user[0])));
             }
           },
-
           child: Padding(
               padding: EdgeInsets.all(10.0),
-              child: CircleImage(img: widget.profileimg, width: 30, height: 30)),
+              child:
+                  CircleImage(img: widget.profileimg, width: 30, height: 30)),
         ),
         InkWell(
-            onTap: (){
-              if(widget.name == "binstar") {
+            onTap: () {
+              if (widget.name == "binstar") {
                 Navigator.push(
-                    context, MaterialPageRoute(builder: (context) =>
-                    MyProfile(user: user[2])));
-              }else  if(widget.name == "jieun") {
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyProfile(user: user[2])));
+              } else if (widget.name == "jieun") {
                 Navigator.push(
-                    context, MaterialPageRoute(builder: (context) =>
-                    MyProfile(user: user[1])));
-              }else if(widget.name == "swlee") {
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyProfile(user: user[1])));
+              } else if (widget.name == "swlee") {
                 Navigator.push(
-                    context, MaterialPageRoute(builder: (context) =>
-                    MyProfile(user: user[3])));
-              }else if(widget.name == "1000bang") {
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyProfile(user: user[3])));
+              } else if (widget.name == "1000bang") {
                 Navigator.push(
-                    context, MaterialPageRoute(builder: (context) =>
-                    MyProfile(user: user[0])));
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyProfile(user: user[0])));
               }
-
-
             },
-
-
-            child: Text(widget.name, style: TextStyle(fontWeight: FontWeight.bold))),
+            child: Text(widget.name,
+                style: TextStyle(fontWeight: FontWeight.bold))),
         Spacer(),
         Padding(
           padding: const EdgeInsets.all(8.0),
@@ -127,38 +151,39 @@ class ListCardState extends State<ListCard> {
     );
   }
 
-  _secondRow(){
-    var alreadySaved = false;
+  _secondRow(var id) {
     return Row(
       children: [
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: InkWell(
-              onTap: (){
-                setState(() {
-                  if(alreadySaved == true){
-                    Icon(FontAwesomeIcons.heart, color: Colors.red,);
-                    widget.likes + 1;
-                   }else if (alreadySaved == false){
-                    alreadySaved = true;
-                    widget.likes - 1;
-                  }
-                });
-              },
-              child: Icon(
-                  alreadySaved ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart,
-                  color: alreadySaved ? Colors.red : null,
-    ),
-    ),),
+            onTap: () {
+              setState(() {
+                print("하트눌러짐");
+                if (alreadySaved == 0) {
+                  print("$alreadySaved 빨간하트로 변경되랏");
+                  alreadySaved = 1;
+                  widget.likes + 1;
+                } else {
+                  alreadySaved = 0;
+                  widget.likes - 1;
+                }
+              });
+            },
+            child: Icon(
+              color: id == alreadySaved ? Colors.black : Colors.red,
+              id == alreadySaved
+                  ? FontAwesomeIcons.heart
+                  : FontAwesomeIcons.solidHeart,
+            ),
+          ),
+        ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: InkWell(
-              onTap: (){
-
+              onTap: () {
                 showDefaultHeightModalBottomSheet(context);
-
               },
-
               child: Icon(FontAwesomeIcons.comment)),
         ),
         Padding(
@@ -170,51 +195,64 @@ class ListCardState extends State<ListCard> {
           padding: const EdgeInsets.all(8.0),
           child: Icon(FontAwesomeIcons.bookmark),
         ),
-
       ],
     );
   }
 
-
-  _thirdRow(){
+  _thirdRow() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          Row(children: [Text(widget.name, style: TextStyle(fontWeight: FontWeight.bold),),
-            SizedBox(width: 10,),
+          Row(children: [
+            Text(
+              widget.name,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              width: 10,
+            ),
             Text(widget.content),
           ]),
-
         ],
       ),
     );
   }
 
-  _fourthRow(){
+  _fourthRow() {
     TextEditingController titleController = TextEditingController();
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10 ),
+      padding: EdgeInsets.symmetric(horizontal: 10),
       child: Row(
         children: [
           CircleImage(img: MyApp.me.profileimgs, width: 25, height: 25),
-          SizedBox(width: 10,),
+          SizedBox(
+            width: 10,
+          ),
           InkWell(
-              onTap: (){
+              onTap: () {
                 showDefaultHeightModalBottomSheet(context);
               },
-              child: Text("댓글추가 ...", style: TextStyle(color: Colors.grey),)),
+              child: Text(
+                "댓글추가 ...",
+                style: TextStyle(color: Colors.grey),
+              )),
           Spacer(),
-          Icon(FontAwesomeIcons.solidHeart, color: Colors.red,),
-          SizedBox(width: 10,),
-          Icon(FontAwesomeIcons.handsClapping, color: Colors.yellow,)
+          Icon(
+            FontAwesomeIcons.solidHeart,
+            color: Colors.red,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(
+            FontAwesomeIcons.handsClapping,
+            color: Colors.yellow,
+          )
         ],
-
       ),
     );
   }
-
-
 
   void showDefaultHeightModalBottomSheet(BuildContext context) {
     showModalBottomSheet(
@@ -222,46 +260,50 @@ class ListCardState extends State<ListCard> {
       builder: (BuildContext context) {
         return SizedBox(
           height: MediaQuery.of(context).size.height * 0.5,
-          child: Column(
-              children: [TextField(
-                cursorColor: Colors.black87,
-                decoration: const InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black87),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black87),
-                  ),
-                  labelStyle: TextStyle(color: Colors.black87),
-                  focusColor: Colors.black87,
-                  labelText: '댓글',
+          child: Column(children: [
+            TextField(
+              cursorColor: Colors.black87,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black87),
                 ),
-                controller: _textEditingController,
-                autofocus: true,
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black87),
+                ),
+                labelStyle: TextStyle(color: Colors.black87),
+                focusColor: Colors.black87,
+                labelText: '댓글',
               ),
-                Row(
-                    children: [Spacer(), Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 5),
-                      child: ElevatedButton(
-
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(Colors.black87),
-                          ),
-                          child: const Text('입력'),
-                          onPressed: (){setState(() {
-                            comments.add(MyComment(time: DateFormat("a K:m").format(new DateTime.now()).replaceAll("AM", "오전")
-                                .replaceAll("PM", "오후") , text: _textEditingController.text));
-                            _textEditingController.clear();
-                            Navigator.pop(context);
-                          });
-                          }
-                      ),
+              controller: _textEditingController,
+              autofocus: true,
+            ),
+            Row(children: [
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.black87),
                     ),
-                    ]             ),
-              ]),
+                    child: const Text('입력'),
+                    onPressed: () {
+                      setState(() {
+                        comments.add(MyComment(
+                            time: DateFormat("a K:m")
+                                .format(new DateTime.now())
+                                .replaceAll("AM", "오전")
+                                .replaceAll("PM", "오후"),
+                            text: _textEditingController.text));
+                        _textEditingController.clear();
+                        Navigator.pop(context);
+                      });
+                    }),
+              ),
+            ]),
+          ]),
         );
       },
     );
   }
-
 }
---
+++ instagram/lib/screens/dm_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../components/circle_Image.dart';
+import '../model/user_info.dart';
+
+class DmScreen extends StatelessWidget {
+  const DmScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text("Direct Message", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black87)),
+        actions: [
+          Icon(FontAwesomeIcons.videoCamera),
+          SizedBox(width: 10,),
+          Icon(FontAwesomeIcons.plus),
+          SizedBox(width: 10,),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 2,),
+            _buildSearchFiled(),
+            _newRecentlyMessage(),
+          ],
+        ),
+      ),
+
+    );
+  }
+  _newRecentlyMessage(){
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+
+
+              Text("메세지", style: TextStyle( fontSize: 16, fontWeight: FontWeight.bold),),
+              Spacer(),
+              Text("요청", style: TextStyle(color: Colors.blueAccent),)
+            ],
+          ),
+          SizedBox(height: 15,),
+          _activeItem(user[1].profileimgs, "정마담", "총 쏘러 갈래?", true),
+          _activeItem(user[0].profileimgs, "손흥민", "오늘 우리 축구함 ㅎ", true),
+          _activeItem(user[2].profileimgs, "하정우", "읽음", false ),
+          _activeItem(user[2].profileimgs, "하정우", "읽음", false ),
+          _activeItem(user[2].profileimgs, "하정우", "읽음", false ),
+          _activeItem(user[2].profileimgs, "하정우", "읽음", false ),
+          _activeItem(user[2].profileimgs, "하정우", "읽음", false ),
+
+
+
+        ],
+
+      ),
+    );
+
+  }
+
+  Widget _activeItem(String img, String name, String message, bool a){
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: [
+          CircleImage(img: img, width: 60, height: 60),
+          SizedBox(width: 10,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name, style: TextStyle(fontWeight: FontWeight.bold),),
+              Text(message),
+            ],
+          ),
+          Spacer(),
+          _circle(a),
+          SizedBox(width: 15,),
+          Icon(FontAwesomeIcons.camera, ),
+
+
+        ],
+      ),
+    );
+  }
+
+  Widget _circle(bool a){
+    if(a == true){
+      return Icon(FontAwesomeIcons.solidCircle, size: 13, color: Colors.blueAccent,);
+    }else {
+      return Container();
+    }
+  }
+
+  Widget _buildSearchFiled() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xFFefefef),
+            border: InputBorder.none,
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                borderSide: BorderSide.none),
+            prefixIcon: Icon(Icons.search),
+            prefixIconColor: Colors.grey,
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            hintText: '검색'),
+      ),
+    );
+  }
+
+
+}
